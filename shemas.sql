c14ebade9348f19ca90207d42b6d8510446703a1
Huyen Nguyen <huyenpromax@Huyens-Mac-Promax.local>
2026-04-16 01:18:48 -0400

    create table enrollment and insert value

--- shemas.sql
+++ shemas.sql
@@ -19,29 +19,58 @@ CREATE TABLE Professor (
 );
 
 -- Insert values into Department 
-INSERT INTO Department VALUES
-(1, 'Computer Science', 'CSC'),
-(2, 'Mathematics', 'MAT'),
-(3, 'Physics', 'PHY'),
-(4, 'Economics', 'ECO'),
-(5, 'English', 'ENG'),
-(6, 'Communication', 'COM');
+INSERT INTO Department VALUES (1, 'Computer Science', 'CSC');
+INSERT INTO Department VALUES (2, 'Mathematics', 'MAT');
+INSERT INTO Department VALUES (3, 'Physics', 'PHY');
+INSERT INTO Department VALUES (4, 'Economics', 'ECO');
+INSERT INTO Department VALUES (5, 'English', 'ENG');
+INSERT INTO Department VALUES (6, 'Communication', 'COM');
 
 -- INSERT PROFESSORS
-INSERT INTO Professor VALUES
-(1, 'John', 'Smith', 'john.smith@uni.edu', 1),
-(2, 'Michael', 'Johnson', 'michael.johnson@uni.edu', 1),
-(3, 'Emily', 'Davis', 'emily.davis@uni.edu', 2),
-(4, 'Sarah', 'Brown', 'sarah.brown@uni.edu', 2),
-(5, 'Daniel', 'Wilson', 'daniel.wilson@uni.edu', 3),
-(6, 'William', 'Thomas', 'william.thomas@uni.edu', 6),
-(7, 'Huyen', 'Nguyen', 'huyen.nguyen@uni.edu', 1),
-(8, 'Luke', 'Prebola', 'luke.prebola@uni.edu', 2),
-(9, 'Tate', 'Seeman', 'tate.seeman@uni.edu', 3),
-(10, 'Gregory', 'Safko', 'gregory.safko@uni.edu', 5);
+INSERT INTO Professor VALUES (1, 'John', 'Smith', 'john.smith@uni.edu', 1);
+INSERT INTO Professor VALUES (2, 'Michael', 'Johnson', 'michael.johnson@uni.edu', 1);
+INSERT INTO Professor VALUES (3, 'Emily', 'Davis', 'emily.davis@uni.edu', 2);
+INSERT INTO Professor VALUES (4, 'Sarah', 'Brown', 'sarah.brown@uni.edu', 2);
+INSERT INTO Professor VALUES (5, 'Daniel', 'Wilson', 'daniel.wilson@uni.edu', 3);
+INSERT INTO Professor VALUES (6, 'William', 'Thomas', 'william.thomas@uni.edu', 6);
+INSERT INTO Professor VALUES (7, 'Huyen', 'Nguyen', 'huyen.nguyen@uni.edu', 1);
+INSERT INTO Professor VALUES (8, 'Luke', 'Prebola', 'luke.prebola@uni.edu', 2);
+INSERT INTO Professor VALUES (9, 'Tate', 'Seeman', 'tate.seeman@uni.edu', 3);
+INSERT INTO Professor VALUES (10, 'Gregory', 'Safko', 'gregory.safko@uni.edu', 5);
+
+-- Create Enrollment table
+DROP TABLE IF EXISTS Enrollment;
+CREATE TABLE Enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    offering_id INT,
+    UNIQUE (student_id, offering_id)
+);
+
+-- Insert values into Enrollment
+INSERT INTO Enrollment VALUES (1, 1, 1);
+INSERT INTO Enrollment VALUES (2, 2, 1);
+INSERT INTO Enrollment VALUES (3, 3, 2);
+INSERT INTO Enrollment VALUES (4, 4, 2);
+INSERT INTO Enrollment VALUES (5, 5, 3);
+INSERT INTO Enrollment VALUES (6, 6, 3);
+INSERT INTO Enrollment VALUES (7, 7, 4);
+INSERT INTO Enrollment VALUES (8, 8, 4);
+INSERT INTO Enrollment VALUES (9, 9, 5);
+INSERT INTO Enrollment VALUES (10, 10, 5);
+INSERT INTO Enrollment VALUES (11, 1, 2);
+INSERT INTO Enrollment VALUES (12, 2, 3);
+INSERT INTO Enrollment VALUES (13, 3, 4);
+INSERT INTO Enrollment VALUES (14, 4, 5);
+INSERT INTO Enrollment VALUES (15, 5, 1);
+INSERT INTO Enrollment VALUES (16, 6, 2);
+INSERT INTO Enrollment VALUES (17, 7, 3);
+INSERT INTO Enrollment VALUES (18, 8, 5);
+INSERT INTO Enrollment VALUES (19, 9, 1);
+INSERT INTO Enrollment VALUES (20, 10, 2);
 
 -- Select all department
 Select * FROM Department;
 Select * FROM Professor;
-
+Select * FROM Enrollment;
 -- Huyen's end
